a2310b5012806573b0ec24b3410661c34fa73e81
Jhonny Vasquez <jhonnyvanckruz@gmail.com>
2016-07-03 10:31:05 -0430

    obteniendo las noticias del json

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,19 @@ var app = angular.module("jhonnynews",[]);
 
 app.controller("news",function($scope,$http){
 
-	
+	$scope.news = [];
+
+	$scope.getnews = function(){		
+		$scope.ancho = "0px";
+
+		$http.get("../../news_mock.json")
+		.success(function(data){
+			$scope.news = data;
+			$scope.ancho = "100%";
+		})
+		.error(function(){
+
+		});
+	}
 	
 });
